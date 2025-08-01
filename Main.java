f3e1b6258031dcfdc6ac6bfe76913454e1dc37d2
JinsanPark <jmfa34@gmail.com>
2025-08-02 01:15:50 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    //백준 1157번
+    //알파벳 입력받기
+    //Hashmap으로 빈도 수 체크. 대소문자 구분 X
+    //가장 많이 사용된 알파벳 대문자로 출력
+    //만일 가장 많이 사용된 알파벳이 2개 이상이면 ?출력
+
+
+    }
+}
