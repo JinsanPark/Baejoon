15461483f45695afc57d6663c94c7d3bb11a7ab3
JinsanPark <jmfa34@gmail.com>
2025-10-16 17:58:38 +0900

    [Silver IV] Title: 좋은 단어, Time: 296 ms, Memory: 22080 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+
+        for (int i = 0; i < N; i++) {
+
+            String input = br.readLine();
+            Stack<Character> stack = new Stack<>();
+
+            for (int j = 0; j < input.length(); j++) {
+
+                char ch = input.charAt(j);
+
+                if (stack.isEmpty() || stack.peek() != ch) {
+                    stack.push(ch);
+                } else if (stack.peek() == ch) {
+                    stack.pop();
+                }
+
+            }
+
+
+            if (stack.isEmpty()) {
+                count++;
+            }
+
+
+        }
+
+        System.out.println(count);
+
+    }
+}
