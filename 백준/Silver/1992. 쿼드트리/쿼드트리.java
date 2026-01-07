9fa827c08a28ea352b556567caf3b388b7c02def
JinsanPark <jmfa34@gmail.com>
2026-01-07 19:13:55 +0900

    [Silver I] Title: 쿼드트리, Time: 176 ms, Memory: 16016 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[][] matrix;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        matrix = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+
+            String inputs = br.readLine();
+
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = inputs.charAt(j) - '0';
+            }
+
+        }
+
+        partition(0,0, N);
+
+    }
+
+    public static void partition(int row, int col, int size) {
+
+        if (check(row, col, size)) {
+            System.out.print(matrix[row][col]);
+            return;
+        }
+
+        System.out.print("(");
+
+        int newSize = size / 2;
+
+        partition(row,col,newSize);
+        partition(row,col + newSize,newSize);
+        partition(row + newSize ,col,newSize);
+        partition(row + newSize ,col + newSize ,newSize);
+
+        System.out.print(")");
+    }
+
+    public static boolean check(int x, int y, int length) {
+
+        for (int i = x; i < x + length; i++) {
+            for (int j = y; j < y + length; j++) {
+
+                if (matrix[x][y] != matrix[i][j]) {
+                    return false;
+                }
+
+            }
+        }
+
+        return true;
+
+    }
+
+}
