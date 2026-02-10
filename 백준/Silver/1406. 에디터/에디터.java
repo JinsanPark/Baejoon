fda998884ec39882176e6a4b388dcd4886795f05
JinsanPark <jmfa34@gmail.com>
2026-02-10 14:59:04 +0900

    [Silver II] Title: 에디터, Time: 392 ms, Memory: 58236 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,110 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int N = str.length();
-        int M = Integer.parseInt(br.readLine());
-        Node head = new Node('?');
-        Node cursor = head;
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
+        String line = br.readLine();
+        int M = Integer.parseInt(br.readLine());
 
-            Node newNode = new Node(str.charAt(i));
+        LinkedList<Character> list = new LinkedList<>();
 
-            newNode.prev = cursor;
-            newNode.next = cursor.next;
-
-            if (cursor.next != null) {
-                cursor.next.prev = newNode;
-            }
-
-            cursor.next = newNode;
-            cursor = newNode;
-
+        for (char c : line.toCharArray()) {
+            list.add(c);
         }
 
+
+        ListIterator<Character> iter = list.listIterator(list.size());
 
         for (int i = 0; i < M; i++) {
 
-            String[] inputs = br.readLine().split(" ");
-            String cmd = inputs[0];
+            String cmd = br.readLine();
+            char c = cmd.charAt(0);
 
-            if (cmd.equals("P")) {
-
-                //input[1]을 커서 왼쪽에 추가하는 로직
-                Node newInput = new Node(inputs[1].charAt(0));
-                newInput.prev = cursor;
-                newInput.next = cursor.next;
-
-                if (cursor.next != null) {
-                    cursor.next.prev = newInput;
-                }
-
-                cursor.next = newInput;
-                cursor = newInput;
-
-
-            } else if (cmd.equals("L")) {
-
-                //커서 왼쪽으로 한칸 움직이는 로직
-                if (cursor.prev != null) {
-                    cursor = cursor.prev;
-                }
-
-            } else if (cmd.equals("D")) {
-
-                //커서 오른쪽으로 한칸 움직이는 로직
-                if (cursor.next != null) {
-                    cursor = cursor.next;
-                }
-
-            } else if (cmd.equals("B")) {
-
-                //커서 왼쪽에 있는 문자 삭제하는 로직.
-                if (cursor != head) {
-                    cursor.prev.next = cursor.next;
-
-                    if (cursor.next != null) {
-                        cursor.next.prev = cursor.prev;
-                    }
-
-                    cursor = cursor.prev;
-
-                }
-            }
+           if (c == 'L') {
+               if (iter.hasPrevious()) {
+                   iter.previous();
+               }
+           } else if (c == 'D') {
+               if (iter.hasNext()) {
+                   iter.next();
+               }
+           } else if (c == 'B') {
+               if (iter.hasPrevious()) {
+                   iter.previous();
+                   iter.remove();
+               }
+           } else if (c == 'P') {
+               char t = cmd.charAt(2);
+               iter.add(t);
+           }
         }
 
-        Node temp = head.next;
-
-        while (temp != null) {
-            sb.append(temp.data);
-            temp = temp.next;
+        for (char ch : list) {
+            sb.append(ch);
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
 
     }
-
-    static class Node {
-        
-        char data;
-        Node next;
-        Node prev;
-
-        Node(char data) {
-
-            this.data = data;
-
-        }
-
-    }
-
-
 }
