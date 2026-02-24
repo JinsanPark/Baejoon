db9320254866fa94ed9d51a6a29a6ba6ca198011
JinsanPark <jmfa34@gmail.com>
2026-02-24 15:45:07 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,53 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 
 public class Main {
 
-    static int N;
-    static int M;
-    static int[][] check;
-    static int count = 0;
+    static int n;
+    static int m;
+    static ArrayList<ArrayList<Integer>> connectedPc;
     static boolean[] visited;
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        String[] input;
-        visited = new boolean[N + 1];
-        check = new int[N + 1][N + 1];
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        connectedPc = new ArrayList<>();
 
-        for (int i = 1; i <= M; i++) {
+        visited = new boolean[n + 1];
 
-            input = br.readLine().split(" ");
-            int x = Integer.parseInt(input[0]);
-            int y = Integer.parseInt(input[1]);
+        for (int i = 0; i <= n; i++) {
+            connectedPc.add(new ArrayList<>());
+        }
 
-            check[x][y] = 1;
-            check[y][x] = 1;
+        StringTokenizer st;
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            connectedPc.get(x).add(y);
+            connectedPc.get(y).add(x);
 
         }
 
+
         dfs(1);
 
-        System.out.println(count);
+        System.out.println(count - 1);
 
     }
 
-    public static void dfs(int a) {
+    public static void dfs(int current) {
 
-        visited[a] = true;
+        visited[current] = true;
+        count++;
 
-        for (int i = 1; i <= N; i++) {
+        for (int next : connectedPc.get(current)) {
 
-            if (check[a][i] == 1 && !visited[i]) {
-                dfs(i);
-                count++;
+            if (!visited[next]) {
+                dfs(next);
             }
 
         }
 
     }
+
+
 }
