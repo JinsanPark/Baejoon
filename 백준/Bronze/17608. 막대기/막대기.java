4134b1b9f3cb3c3bb9cdd6bd134a14287ac24428
JinsanPark <jmfa34@gmail.com>
2026-03-03 11:06:18 +0900

    [Bronze II] Title: 막대기, Time: 208 ms, Memory: 23208 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,27 +8,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        Deque<Integer> stack = new ArrayDeque<>();
+        int[] list = new int[N];
         int count = 0;
         int stick = Integer.MIN_VALUE;
 
         for (int i = 0; i < N; i++) {
             int a = Integer.parseInt(br.readLine());
-            stack.push(a);
+            list[i] = a;
         }
 
-        while (!stack.isEmpty()) {
 
-            int a = stack.pop();
+
+        for (int i = N - 1; i >= 0; i--) {
+
+            int a = list[i];
 
             if (a > stick) {
                 stick = a;
                 count++;
             }
-
         }
 
+
         System.out.println(count);
+
 
     }
 }
